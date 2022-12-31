a9dd7908cc10d97c7b8968f6101f3fc89e78ad56
khumainisme <khumainisme@gmail.com>
2023-01-01 02:23:14 +0700

    first commit

---
+++ script/Script.js
@@ -0,0 +1,14 @@
+// Smooth Scroll
+// $('a[href^="#"]').on("click", function (e) {
+//   // e.preventDefault();
+
+//   const target = this.hash,
+//     $target = $(target);
+
+//   $("html, body").stop().animate({
+//     "scrollTop": $target.offset().top - 78
+//   }, 400, "swing", function () {
+//     window.location.hash = target;
+//   });
+// });
+// End Smooth Scroll
